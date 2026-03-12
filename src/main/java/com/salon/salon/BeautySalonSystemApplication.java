d315142e2933c539c2f53f764eb28f24badb5ff5
IT25200210 <it25200210@my.sliit.lk>
2026-03-12 10:57:06 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/salon/salon/BeautySalonSystemApplication.java
@@ -0,0 +1,13 @@
+package com.salon.salon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySalonSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautySalonSystemApplication.class, args);
+    }
+
+}
